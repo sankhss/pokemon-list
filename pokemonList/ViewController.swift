f81b510601aacdf25aac162fbcca33224fb91b41
Samuel Silva <samuelh.dev@gmail.com>
2022-07-31 00:18:43 -0300

    Initial Commit

---
+++ pokemonList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemonList
+//
+//  Created by Samuel Silva on 31/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
